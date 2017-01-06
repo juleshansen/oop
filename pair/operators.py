6f72745280d6136fe3b3df8160e7c7d0889d9774
J.F. Omhover <jf.omhover@galvanize.com>
2017-01-06 11:44:06 -0800

    better comments

--- pair/operators.py
+++ pair/operators.py
@@ -3,23 +3,35 @@
 
 class UnionShape():
     def __init__(self, a, b):
+        """Constructs a UnionShape as union of given shapes a and b.
+        Note: color or union is average between colors of a and b."""
         pass
 
     def f(self, x, y):
+        """Characteristic function of the shape.
+        Returns True if (x,y) is inside the shape, else False."""
         pass
 
 
 class IntersectionShape():
     def __init__(self, a, b):
+        """Constructs a IntersectionShape as union of given shapes a and b.
+        Note: color or intersection is average between colors of a and b."""
         pass
 
     def f(self, x, y):
+        """Characteristic function of the shape.
+        Returns True if (x,y) is inside the shape, else False."""
         pass
 
 
 class DiffShape():
     def __init__(self, a, b):
+        """Constructs a DiffShape as union of given shapes a and b.
+        Note: color or difference is the color of a."""
         pass
 
     def f(self, x, y):
+        """Characteristic function of the shape.
+        Returns True if (x,y) is inside the shape, else False."""
         pass
--- pair/shapes.py
+++ pair/shapes.py
@@ -5,21 +5,30 @@ import math
 
 class Circle():
     def __init__(self, cx, cy, radius, color):
+        """Constructs a Circle instance
+        centered around (cx,cy) with radius and color."""
         self.cx = cx
         self.cy = cy
         self.radius = radius
         self.color = color
 
     def f(self, x, y):
+        """Characteristic function of the shape.
+        Returns True if (x,y) is inside the shape, else False."""
         t_dist = math.sqrt((x - self.cx)**2 + (y - self.cy)**2)
         if (t_dist < self.radius):
             return(True)
         else:
             return(False)
 
+
 class Rectangle():
     def __init__(self, x0, y0, x1, y1, color):
+        """Constructs a Rectangle instance
+        with top-left point (x0,y0), bottom-right (x1,y1) and color."""
         pass
 
     def f(self, x, y):
+        """Characteristic function of the shape.
+        Returns True if (x,y) is inside the shape, else False."""
         pass
