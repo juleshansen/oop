5a0f3606e1a53ca8f6eca17a1c42b7be6883cf13
J.F. Omhover <jf.omhover@galvanize.com>
2017-02-21 10:02:34 -0800

    better commenting

--- individual/shapescriptparser.py
+++ individual/shapescriptparser.py
@@ -6,6 +6,18 @@ from operators import UnionShape, IntersectionShape, DiffShape
 from collections import OrderedDict
 
 class ShapeScriptParser():
+    """A parser for a scripting language
+    that would generate shapes (as objects defined in shapes.py)
+    and display the result on screen (using SimpleGUI).
+
+    Parameters
+    ----------
+    gui : {SimpleGUI} a gui object for drawing pixels.
+
+    Attributes
+    ----------
+    TBD
+    """
     def __init__(self, gui):
         """instanciates a shape factory, gui is provided for future drawing"""
         pass
